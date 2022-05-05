4a9a3494ced224c804c9e207682e51d9be21ee09
priyankadhatrak <priyanka.dhatrak@sts.local>
2022-05-05 12:49:48 +0530

    initialization

--- learning/basicprograms/datatypes.py
+++
@@ -1,9 +0,0 @@
-a="hello"
-x=1j
-t=("apple","ball","cat")
-l=["apple","ball","cat"]
-d={"name": "john", "age":36}
-s={"apple","ball","cat"}
-fs= frozenset({"apple","ball","cat"})
-
-print("string:",a,"\ncomplex: ",x,"\ntuple: ",t,"\nlist: ",l,"\ndictionary: ",d,"\nset: ",s,"\nFrozenset: ",fs)
--- learning/basicprograms/re_functions.py
+++
@@ -1,7 +0,0 @@
-import re
-
-
-str="i live in mumbai and mumbai is part of india."
-x=re.search("mumbai",str)
-#print(str)
-print(x)
